1b4fc202323b3a71bfb8194f2ffb39f7644c3cb4
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-09-20 16:29:10 +0300

    fixed link

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <BrowserRouter basename="/Rent-car">
+  <BrowserRouter basename="/rent-car">
     <App />
   </BrowserRouter>
 );
